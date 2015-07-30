13130edf148b4ca16c5cce36cf2b14b921ff5302
Sebastian Alfers <sa@plista.com>
2015-07-30 07:40:13 +0200

    ensure results are comparable with spark

--- random-projection/error_distances_python.py
+++ random-projection/error_distances_python.py
@@ -19,12 +19,12 @@ rows = []
 cols = []
 values = []
 matrix_size = 100
-rng = range(0, matrix_size-1)
-for x in rng:
+rng = range(0, matrix_size)
+for x in range(0, matrix_size-1):
     for y in rng:
         rows.append(x)
         cols.append(y)
-        values.append(float(x+1))
+        values.append(np.float64(x+1))
 
 matrix = csr_matrix( (values,(rows,cols))).todense()
 
@@ -35,6 +35,7 @@ orig_dimension = np.shape(matrix)[1]
 print("dataset rows: %s" % np.shape(matrix)[0])
 print("dataset columns: %s" % orig_dimension)
 
+# claculate the euclidean distance
 def euclideanDist(a,b):
     sum = 0.0
     for item in zip(a,b):
@@ -61,40 +62,34 @@ def evaluatePairwiseDistances(dataset, intrinsicDimension):
     rand_matrix = scikit_rp.getSparseRP(intrinsicDimension)._make_random_matrix(intrinsicDimension, orig_dimension)
     # perform the reduction
     reduced_matrix = dataset * rand_matrix.transpose()
-
     amount_orig, dist = getPairwiseDist(matrix)
-
     # call method from above
     amount_reduced, reduced_dist = getPairwiseDist(reduced_matrix)
-
-    sumOrig = 0.0
-    sumReduced = 0.0
-    sumError = 0.0
+    sumOrig = []
+    sumReduced = []
+    sumError = []
     keysOrig = dist.iterkeys()
     keysReduced = reduced_dist.iterkeys()
     for keyz in zip(keysOrig, keysReduced):
         orig = keyz[0]
         reduced = keyz[1]
-
         orig_dist_value = dist[orig]
         reduced_dist_value = reduced_dist[reduced]
-
         if orig_dist_value > reduced_dist_value:
             error = orig_dist_value - reduced_dist_value
         else:
             error = reduced_dist_value - orig_dist_value
-
         if orig != reduced:
             raise "error. '%s' must be equal to '%s'" % (orig, reduced)
+        # print "#%s orig dist: %s" % (orig, orig_dist_value)
+        sumOrig.append(orig_dist_value)
+        sumReduced.append(reduced_dist_value)
+        sumError.append(error)
 
-        sumOrig = sumOrig + orig_dist_value
-        sumReduced = sumReduced + reduced_dist_value
-        sumError = sumError + error
-
-    return (sumOrig, sumReduced, sumError)
+    return (np.mean(sumOrig), np.mean(sumReduced), np.mean(sumError))
 
 avg_error = 0.0
-folds = range(0, 30)
+folds = range(0, 5)
 x = []
 y_orig = []
 y_reduced = []
@@ -111,6 +106,7 @@ for dimension in np.arange(5, 16, 1):
         orig_sum.append(orig)
         reduced_sum.append(reduced)
         # print error_sum
+
     y_orig.append(np.mean(orig_sum))
     y_reduced.append(np.mean(reduced_sum))
     y_error.append(np.mean(error_sum))
